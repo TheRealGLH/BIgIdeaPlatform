45d638884b15d69e3fb80a8ecd5bdd433053c76c
Daniels,Martijn M.J.S <martijn.daniels@student.fontys.nl>
2019-11-28 21:20:07 +0100

    annonates Shared tests

--- PlatformGameShared/src/test/java/SharedClasses/SpriteUpdateTest.java
+++ PlatformGameShared/src/test/java/SharedClasses/SpriteUpdateTest.java
@@ -20,54 +20,64 @@ public class SpriteUpdateTest {
 
     @BeforeEach
     public void setUp() {
+        System.out.println("Setting up...");
         sprite = new SpriteUpdate(objectNr, position, size, updateType, spriteType, isFacingLeft);
     }
 
     @Test
     public void getObjectNr() {
+        System.out.println("Testing SpriteUpdate objectNr getter");
         Assert.assertEquals(objectNr, sprite.getObjectNr());
     }
 
     @Test
     public void getPosition() {
+        System.out.println("Testing SpriteUpdate position getter");
         Assert.assertEquals(position, sprite.getPosition());
     }
 
     @Test
     public void getSize() {
+        System.out.println("Testing SpriteUpdate size getter");
         Assert.assertEquals(size, sprite.getSize());
     }
 
     @Test
     public void getSpriteType() {
+        System.out.println("Testing SpriteUpdate spritetype getter");
         Assert.assertEquals(spriteType, sprite.getSpriteType());
     }
 
     @Test
     public void getUpdateType() {
+        System.out.println("Testing SpriteUpdate UpdateType getter");
         Assert.assertEquals(updateType, sprite.getUpdateType());
     }
 
     @Test
     public void isFacingLeft() {
+        System.out.println("Testing SpriteUpdate isFacingLeft getter");
         Assert.assertEquals(isFacingLeft, sprite.isFacingLeft());
     }
 
     @Test
-    public void testEqualsDifferentType(){
+    public void testEqualsDifferentType() {
+        System.out.println("Testing SpriteUpdate equals() different type");
         Object other = new Object();
         Assert.assertNotEquals(sprite, other);
     }
 
     @Test
-    public void testEqualsSameId(){
-        SpriteUpdate other = new SpriteUpdate(sprite.getObjectNr(),Vector2.Zero(),Vector2.Zero(),SpriteUpdateType.CREATE,SpriteType.PLATFORM,true);
+    public void testEqualsSameId() {
+        System.out.println("Testing SpriteUpdate equals() same ID");
+        SpriteUpdate other = new SpriteUpdate(sprite.getObjectNr(), Vector2.Zero(), Vector2.Zero(), SpriteUpdateType.CREATE, SpriteType.PLATFORM, true);
         Assert.assertEquals(sprite, other);
     }
 
     @Test
-    public void testEqualsDifferentId(){
-        SpriteUpdate other = new SpriteUpdate(sprite.getObjectNr()+2,Vector2.Zero(),Vector2.Zero(),SpriteUpdateType.CREATE,SpriteType.PLATFORM,true);
+    public void testEqualsDifferentId() {
+        System.out.println("Testing SpriteUpdate equals() different ID");
+        SpriteUpdate other = new SpriteUpdate(sprite.getObjectNr() + 2, Vector2.Zero(), Vector2.Zero(), SpriteUpdateType.CREATE, SpriteType.PLATFORM, true);
         Assert.assertNotEquals(sprite, other);
     }
 }
