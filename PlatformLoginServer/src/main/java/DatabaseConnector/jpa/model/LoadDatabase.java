31339c9e79d22dae1c424cf7c32bca6d54ae94a8
Daniels,Martijn M.J.S <martijn.daniels@student.fontys.nl>
2020-06-13 15:37:06 +0200

    Attempt at getting bean to actually work

--- PlatformLoginServer/src/main/java/DatabaseConnector/jpa/model/LoadDatabase.java
+++ PlatformLoginServer/src/main/java/DatabaseConnector/jpa/model/LoadDatabase.java
@@ -3,15 +3,19 @@ package DatabaseConnector.jpa.model;
 import DatabaseConnector.jpa.PlayerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class PlayerRepoApp {
+
+@Configuration
+public class LoadDatabase {
 
 
     @Bean
     CommandLineRunner initDatabase(PlayerRepository repository) {
         return args -> {
-            repository.save(new Player("Mr Test", "letmein123"));
-            repository.save(new Player("Mrs Test", "letmein123"));
+            System.out.println("Filling db: ");
+            System.out.println(repository.save(new Player("Mr Test", "letmein123")));
+            System.out.println(repository.save(new Player("Mrs Test", "letmein123")));
 
             for (Player player : repository.findAll()) {
                 System.out.println("Player: " + player.getName());
--- PlatformLoginServer/src/main/java/io/swagger/Swagger2SpringBoot.java
+++ PlatformLoginServer/src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -4,12 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = { "io.swagger", "io.swagger.api" , "io.swagger.configuration"})
+//@ComponentScan(basePackages = { "io.swagger", "io.swagger.api" , "io.swagger.configuration"})
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
